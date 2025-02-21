4a8fb51f8c09d270c643bf3ccc147903f04245f3
Alex Sink <alexsink@yahoo.com>
2025-02-20 17:59:10 -0800

    revert main page

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 30249026,
+    wasmFileSize: 26073254,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
